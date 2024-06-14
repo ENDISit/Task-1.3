01cc88b5ebc776542b6741d5c5f9603dabf3fb4b
ENDISit <mr.ilia08@mail.ru>
2024-06-14 22:42:39 +0300

    Добавьте файлы проекта.

---
+++ Task 1.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Нечетные числа из массива:");
+        foreach (int number in numbers)
+        {
+            if (number % 2 != 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
